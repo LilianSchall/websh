a580cbc0b5e814c8198aecc0b92f4271a5d8d721
Lilian Schall <lilian.schall@epita.fr>
2026-05-01 17:30:55 +0200

    feat: added complete_command node

--- shell/src/parser/complete_command.rs
+++ shell/src/parser/complete_command.rs
@@ -1,8 +1,20 @@
 use super::list::List;
 use super::separator::Separator;
 
+use crate::lexer::Lexer;
+use crate::parser::parseable::{Parseable, ParseError};
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct CompleteCommand {
 	pub list: List,
 	pub separator: Option<Separator>,
+}
+
+impl Parseable for crate::parser::complete_command::CompleteCommand {
+    fn parse(lexer: &mut Lexer) -> Result<Option<Self>, ParseError> where Self: Sized {
+        let list = List::parse(lexer)?.unwrap();
+        // Separator is optional, so we ignore parsing errors
+        let separator = Separator::parse(lexer).ok().unwrap_or(None); 
+        Ok(Some(CompleteCommand { list, separator }))
+    }
 }
